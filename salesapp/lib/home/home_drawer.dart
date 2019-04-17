dde2a507c553b930e3b2d0b9e4966d0e99977409
luciole <chenwei@expservice.com.cn>
2019-04-17 11:45:02 +0800

    funtion update: picker image by local gallery or camera

--- salesapp/lib/home/home_drawer.dart
+++ salesapp/lib/home/home_drawer.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:salesapp/main/public.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class HomeDrawerScene extends StatelessWidget {
+class HomeDrawerScene extends StatefulWidget {
+  @override
+  HomeDrawerSceneState createState() => HomeDrawerSceneState();
+}
+
+class HomeDrawerSceneState extends State<HomeDrawerScene> {
   final List<String> _gridItemTitle = [
     '首页',
     '客源',
@@ -32,6 +40,23 @@ class HomeDrawerScene extends StatelessWidget {
     'img/set_item.png'
   ];
 
+  File _image;
+
+  Future getImageByCamera() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future getImageByGallery() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Column(
@@ -43,38 +68,40 @@ class HomeDrawerScene extends StatelessWidget {
           child: new Row(
             children: <Widget>[
               new SizedBox(width: 10),
-              new Container(
-                width: 64,
-                height: 64,
-                child: new Image.asset(''),
+              new GestureDetector(
+                onTap: () => {_showDialog(context)},
+                child: new Container(
+                  width: 64,
+                  height: 64,
+                  child: _image == null ? Text('NO Image selected') : Image.file(_image),
+                ),
               ),
               new SizedBox(width: 10,),
               new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
+                  new SizedBox(height: 12,),
                   new Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       new Text(
                         '左小青',
                         textAlign: TextAlign.left,
                         style: new TextStyle(
-                            fontSize: 20, color: SAColor.textBlack,
+                          fontSize: 20,
+                          color: SAColor.textBlack,
                         ),
                       ),
+                      new SizedBox(width: 40,),
                       new GestureDetector(
-                        onTap: () => {},
-                        child: new Image.asset(
-                          '',
-                          width: 80,
-                          height: 20,
-                        ),
-                      )
+                          onTap: () => {}, child: new Text('个人资料'))
                     ],
                   ),
                   new Text(
                     '上海申银汽车服务有限公司',
                     textAlign: TextAlign.left,
                     style:
-                    new TextStyle(fontSize: 13, color: SAColor.textBlack),
+                        new TextStyle(fontSize: 13, color: SAColor.textBlack),
                   )
                 ],
               )
@@ -87,7 +114,7 @@ class HomeDrawerScene extends StatelessWidget {
         new GridView.count(
           crossAxisCount: 3,
           scrollDirection: Axis.vertical,
-          shrinkWrap: true ,
+          shrinkWrap: true,
           primary: false,
           children: List.generate(12, (index) {
             return gridItemWidget(index);
@@ -97,14 +124,48 @@ class HomeDrawerScene extends StatelessWidget {
     );
   }
 
+  void _showDialog(BuildContext cxt) {
+    showCupertinoModalPopup(
+        context: cxt,
+        builder: (cxt) {
+          var dialog = CupertinoActionSheet(
+            cancelButton: CupertinoActionSheetAction(
+                onPressed: () {
+                  Navigator.pop(cxt);
+                }, child: new Text('取消')),
+            actions: <Widget>[
+              CupertinoActionSheetAction(
+                  onPressed: () {
+                    Navigator.pop(cxt);
+                    getImageByCamera();
+                  },
+                  child: Text('拍照')),
+              CupertinoActionSheetAction(
+                  onPressed: () {
+                    Navigator.pop(cxt);
+                    getImageByGallery();
+                  },
+                  child: Text('从相册中选择')),
+            ],
+          );
+          return dialog;
+        });
+  }
+
   Widget gridItemWidget(index) {
     return new Container(
       width: 60,
       height: 50,
       child: new Column(
         children: <Widget>[
-          new Image.asset(_gridItemImgUrl[index], width: 20, height: 20,),
-          new SizedBox(height: 10,),
+          new Image.asset(
+            _gridItemImgUrl[index],
+            width: 20,
+            height: 20,
+          ),
+          new SizedBox(
+            height: 10,
+          ),
           new Text(
             _gridItemTitle[index],
             style: new TextStyle(
